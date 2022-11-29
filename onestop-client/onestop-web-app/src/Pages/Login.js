7bf351ca336dee401985c61ee34d57fb62f693d2
Gauravmg6 <gaurav.mg@hotmail.com>
2022-11-29 18:06:23 -0500

    made changes in login style

--- onestop-client/onestop-web-app/src/Pages/Login.js
+++ onestop-client/onestop-web-app/src/Pages/Login.js
@@ -15,21 +15,21 @@ const Login = ({closeClick}) => {
                     <>
                     <h2>{signUp?"Sign Up":"Login"}</h2>
                     <form action="">
-                        <input type="text" placeholder='Enter Full Name' />
-                        <input type="text" placeholder='Enter Mobile Number' />
-                        <input type="text" placeholder='Enter Email' />
-                        <input type="text" placeholder='Enter Password' />
-                        <input type="text" placeholder='Confirm Password' /><br />
-                        <button id='submit'>Sign Up</button>
+                        <input className='form-control' type="text" placeholder='Enter Full Name' />
+                        <input className='form-control' type="text" placeholder='Enter Mobile Number' />
+                        <input className='form-control' type="text" placeholder='Enter Email' />
+                        <input className='form-control' type="text" placeholder='Enter Password' />
+                        <input className='form-control' type="text" placeholder='Confirm Password' /><br />
+                        <button className='btn btn-dark' id='submit'>Sign Up</button>
                     </form>
                     </>
                 ):(
                 <>
                 <h2>Login</h2>
                 <form action="">
-                    <input type="text" placeholder="Username" />
-                    <input type="text" placeholder='Password' /><br />
-                    <button id='submit'>Login</button>
+                    <input className='form-control' type="text" placeholder="Username" />
+                    <input className='form-control' type="text" placeholder='Password' /><br />
+                    <button className='btn btn-dark' id='submit'>Login</button>
                 </form></>
                 )}
                 <button id='signup' onClick={()=> setSignup(!signUp)}>{signUp?"Have an account? Login":"Don't have an account? Sign Up"}</button>
