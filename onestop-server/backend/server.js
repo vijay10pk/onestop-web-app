a23ff5641d60f996d273f685845c8eb55020d0ba
Vijay Anand <vijay10pk@gmail.com>
2022-11-29 18:00:22 -0500

    user authentication backend changes

---
+++ onestop-server/backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
+const port = process.env.PORT || 5000
+const connectDB = require('./config/db')
+
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false}))
+
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
+
+app.listen(port, ()=> console.log(`Server started at port ${port}`))
