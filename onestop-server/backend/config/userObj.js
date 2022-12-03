16768ca854ab2c2831929ce91c806770e534b28b
Gauravmg6 <gaurav.mg@hotmail.com>
2022-12-03 13:57:31 -0500

    complete backend

---
+++ onestop-server/backend/config/userObj.js
@@ -0,0 +1,11 @@
+class userObj{
+    constructor(obj){
+        this.userName = obj.userName,
+        this.password = obj.password,
+        this.phoneNumber = obj.phoneNumber,
+        this.email = obj.email,
+        this.fullName = obj.fullName
+    }
+}
+
+module.exports = userObj 
--- onestop-server/backend/models/serviceModal.js
+++ onestop-server/backend/models/serviceModal.js
@@ -1,19 +1,65 @@
-const mongoose = require('mongoose');
+const collection = require('../config/connection');
+const serviceDatabase=require('../models/services.json');
+let service = {}
 
-const serviceSchema = mongoose.Schema({
-    service: {
-        sub_service: [{
-            _id: { type: String },
-            name: { type: String },
-            description: { type: String },
-            category: { type: String },
-            price: { type: Number },
-            image: { type: String },
-        }]
-    },
-},
-{
-    timestamps: true,
-})
+service.setupDB = () => {
+    return collection.getProductCollection().then( prodColl => {
+        return prodColl.deleteMany().then( () => {
+            return prodColl.insertMany(serviceDatabase).then((data) => {
+                console.log(data)
+                if (data) return data
+                else return null
+            });
+        } );
+    } );
+ }
 
-module.exports = mongoose.model('Service', serviceSchema)
+service.getCategory=(category)=>{
+    return collection.getProductCollection().then((model)=>{
+        // console.log("i'm here")
+        return model.find({"category":category},{_id:0}).then((data)=>{
+            if(data!=0){
+                // console.log(data)
+                 return data
+            }
+            else {
+                return null;
+            }
+        })
+    })
+}
+
+
+service.getSearch=(search)=>{
+    return collection.getProductCollection().then((model)=>{
+        return model.find({}, { _id: 0 }).then((data) => {
+            var list = []
+            var objarr =[]
+            data.map((obj) => {
+                let cat = obj.sub_category
+                cat.forEach((sub) => {
+                    var b = sub.services
+                    b.forEach((n) => {
+                        objarr.push(n)
+                    })
+                })
+            })
+            objarr.map((obj)=>{
+                var name=obj.name.split(" ");
+                name.push(obj.category);
+                for(let i of name){
+                    if(i.toLowerCase()===search.toLowerCase()){
+                        list.push(obj);
+                        break;
+                    }
+                }
+            })
+            if(list!=0){
+                 return list
+            }
+            else throw new Error("Data is not Available for your search")
+        })
+    })
+}
+
+module.exports = service
